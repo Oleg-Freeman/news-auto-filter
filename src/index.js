e09609248a277f4755cf2cd4af766f78623884a7
Oleg Voitiuk <youremail@example.com>
2026-09-08 19:33:36 +0300

    Test function

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 export async function GET(request) {
   const response = await fetch('https://api.vercel.app/products');
   const products = await response.json();
+  console.log('TEST - ', products);
   return Response.json(products);
 }
