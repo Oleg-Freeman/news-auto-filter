9dc993b3fd528315c9d00038df1055fdf0765b8a
Oleg Voitiuk <youremail@example.com>
2026-09-08 19:52:50 +0300

    Main handler

--- api/index.js
+++ api/index.js
@@ -1,6 +1,16 @@
-export async function GET(request) {
-  const response = await fetch('https://api.vercel.app/products');
-  const products = await response.json();
-  console.log('TEST - ', products);
-  return Response.json(products);
+export default async function handler(req, res) {
+  const authHeader = res.headers.authorization;
+
+  if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
+    return res.status(401).json({ error: 'Unauthorized' });
+  }
+
+  console.log('Authentication header is ', authHeader);
+
+  try {
+    return res.status(200).json({ success: true });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: error.message });
+  }
 }
